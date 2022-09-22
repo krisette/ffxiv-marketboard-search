7a5c3fac7a41d8858b0ca9b5e246c852b4dc83ed
Krisette Odegard <kmodeg@gmail.com>
2022-09-22 08:47:11 -0700

    added item pictures, more styling

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(urlXIVAPI)
     .then(response => response.json())
     .then(data => {
-      itemIds = data.Results[0].ID});
+      itemIds = data.Results[0].ID;
+      iconURL = data.Results[0].Icon;});
 
     const urlUniversalis = `https://universalis.app/api/v2/${worldDcRegion}/${itemIds}?listings=10`;
     fetch(urlUniversalis)
@@ -24,31 +25,35 @@ document.addEventListener('DOMContentLoaded', () => {
         const listings = data['listings'];
         if (data['listings'] === undefined) {
           const div = document.createElement('div');
-          div.setAttribute('class', 'listing')
-          const noPostings = document.createElement('span')
-          noPostings.setAttribute('id', 'noPostings')
-          noPostings.innerText = 'No Postings on Auction House';
+          div.setAttribute('class', 'listing');
+          const noPostings = document.createElement('span');
+          noPostings.setAttribute('id', 'noPostings');
+          noPostings.setAttribute('style', 'color: #791723; font-weight: bold; text-align: center;');
+          noPostings.innerText = 'No Postings on Market Board';
           div.append(noPostings);
           document.querySelector('.results').append(div)
         }
-        console.log(listings);
+        // console.log(listings);
         for (let i = 0; listings.length > 0; i++) {
           const div = document.createElement('div');
-          // give the div a class of .listing
           div.setAttribute('class', 'listing');
-          // create span element with id 'username'
-          const pricePerUnit = document.createElement('span');
+          const iconDiv = document.createElement('div');
+          iconDiv.setAttribute('class', 'itemIcon');
+          const icon = document.createElement('img');
+          icon.setAttribute('src', 'http://xivapi.com' + iconURL);
+          iconDiv.append(icon);
+          const pricePerUnit = document.createElement('div');
           pricePerUnit.setAttribute('id', 'pricePerUnit');
-          const quantity = document.createElement('span');
+          const quantity = document.createElement('div');
           quantity.setAttribute('id', 'quantity');
-          const worldName = document.createElement('span');
+          const worldName = document.createElement('div');
           worldName.setAttribute('id', 'worldName');
           // assign the innerText of the messages to the current data's message key value
-          pricePerUnit.innerText = `Price: ${listings[i].pricePerUnit}`
-          quantity.innerText = `Quantity: ${listings[i].quantity}`;
-          worldName.innerText = `World: ${listings[i].worldName}`;
+          pricePerUnit.innerHTML = `Price: <b>${listings[i].pricePerUnit}</b>`
+          quantity.innerHTML = `Quantity: <b>${listings[i].quantity}</b>`;
+          worldName.innerHTML = `World: <b>${listings[i].worldName}</b>`;
           // append the div to the div with the id #chatbox
-          div.append(pricePerUnit, quantity, worldName);
+          div.append(iconDiv, pricePerUnit, quantity, worldName);
           document.querySelector('.results').append(div);
         }
       })
